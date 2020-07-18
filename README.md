04cd6aa61b11f36a4185d4763f5bdae4b4f69052
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-07-18 17:53:52 -0400

    added text area to time blocks and updated manual time push

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DayPlanner
 Week 5 Homework - Day Planner
+
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+Used [Moment.js](https://momentjs.com/) for time format
--- script.js
+++ script.js
@@ -1,49 +1,71 @@
 //open the planner: current date and time is displayed
+var currentHour = moment().hour(); // Number
+
+// Store startingHour
+var date = new Date();
+var startingHour = date.getHours();
+
 //created function for consistent time update
 function renderClock() {
-    $("#currentTime").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+  $("#currentTime").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+  if (currentHour > startingHour) {
+      updateTimeBlocks()
+  }
 }
 
-setInterval(renderClock, 1000)
+$(document).keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        currentHour++  
+    }
+});
+
+setInterval(renderClock, 1000);
+
 
 //create a time block
 var timeBlockMarkup = "<div></div>";
-for (var i=1; i <= 24; i++) {
-    var hourMarkup = `${i} am`;
-    if (i == 12) {
-        hourMarkup = `${i} pm`;
+for (var i = 1; i <= 24; i++) {
+  var hourMarkup = `${i} am`;
+  if (i == 12) {
+    hourMarkup = `${i} pm`;
+  }
+  if (i > 12) {
+    hourMarkup = `${i - 12} pm`;
+    if (i === 24) {
+      hourMarkup = "12 am";
     }
-    if (i > 12) {
-        hourMarkup = `${i-12} pm`; 
-        if (i === 24) {
-            hourMarkup = "12 am";
-        }
-    }
-    timeBlockMarkup += 
-    `<div class="row"> 
+  }
+  timeBlockMarkup += `<div class="row"> 
     <div class="col-sm-1 hourCol" id="${i}">${hourMarkup}</div>
-    <div class="col-sm-10 inputCol" id="input-${i}"></div>
+    <div class="col-sm-10 inputCol" id="input-${i}"><textarea id="input-${i}-text" style="width: 100%; border-left: 0px !important;"></textarea></div>
     <div class="col-sm-1 saveBtn" id="saveBtn-${i}"></div>
-    </div>`
+    </div>`;
 }
-$("#timeBlockContainer").append(timeBlockMarkup)
-
+$("#timeBlockContainer").append(timeBlockMarkup);
+updateTimeBlocks();
 //color code time block for past, present and future
-var currentHour = moment().hour(); // Number
-console.log(currentHour)
-$(`#input-${currentHour}`).addClass("present")
+
+console.log(currentHour);
 
 // Use for loop to add past and future color coding
-for (var i = 1; i <= 24; i++) {
+function updateTimeBlocks() {
+  for (var i = 1; i <= 24; i++) {
+    //default remove all
+    $(`#input-${i}`).removeClass("past present future")
+    
     if (i < currentHour) {
-        $(`#input-${i}`).addClass("past")
+      $(`#input-${i}`).addClass("past");
     }
+
+    $(`#input-${currentHour}`).addClass("present");
+
     if (i > currentHour) {
-        $(`#input-${i}`).addClass("future")
+      $(`#input-${i}`).addClass("future");
     }
+  }
 }
-
-
 
 //user can enter an event
 
@@ -53,4 +75,6 @@ for (var i = 1; i <= 24; i++) {
 
 //refresh the page and saved information stays in place
 
+//create text areas in time blocks
+// .val of that input when save button is pressed
 
